e4961f2265253e6c1cf86393fd0494b65483d709
KayongoYongo <sknsammie10@gmail.com>
2024-09-05 21:25:14 +0300

    class

--- class.py
+++ class.py
@@ -1,3 +1,22 @@
-class Mercedes:
-    pass
+#!/bin/python3
 
+class Car:
+    def __init__(self, name):
+        self.name = name
+
+# First instance of a car
+mazda = Car()
+
+# Second instance of a car
+honda = Car()
+
+print(mazda)
+print(honda)
+
+"""
+After printing, the two instances will be
+stored in separate areas of memory. These two instances
+of a car are considered different by default.
+"""
+
+print(f'{honda.__name__} == {mazda.__name__} : Result{honda == mazda}')
